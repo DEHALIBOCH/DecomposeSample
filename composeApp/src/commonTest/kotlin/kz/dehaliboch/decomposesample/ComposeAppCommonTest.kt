6510f093f8778a21b67191c78ed1e15cb2e84193
dehaliboch <almabekov.marat@gmail.com>
2025-10-03 18:29:49 +0500

    Initial commit

---
+++ composeApp/src/commonTest/kotlin/kz/dehaliboch/decomposesample/ComposeAppCommonTest.kt
@@ -0,0 +1,12 @@
+package kz.dehaliboch.decomposesample
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class ComposeAppCommonTest {
+
+    @Test
+    fun example() {
+        assertEquals(3, 1 + 2)
+    }
+}
---
+++ composeApp/src/iosMain/kotlin/kz/dehaliboch/decomposesample/MainViewController.kt
@@ -0,0 +1,5 @@
+package kz.dehaliboch.decomposesample
+
+import androidx.compose.ui.window.ComposeUIViewController
+
+fun MainViewController() = ComposeUIViewController { App() }
