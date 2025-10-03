7d1b1098273137c184ecb242f10d6001ee18cf77
dehaliboch <almabekov.marat@gmail.com>
2025-10-03 20:58:51 +0500

    implemented navigation in screens

--- composeApp/src/androidMain/kotlin/kz/dehaliboch/decomposesample/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/kz/dehaliboch/decomposesample/MainActivity.kt
@@ -4,22 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
+import com.arkivanov.decompose.retainedComponent
+import kz.dehaliboch.decomposesample.navigation.RootComponent
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         enableEdgeToEdge()
         super.onCreate(savedInstanceState)
 
+        val root = retainedComponent {
+            RootComponent(it)
+        }
+
         setContent {
-            App()
+            App(root)
         }
     }
-}
-
-@Preview
-@Composable
-fun AppAndroidPreview() {
-    App()
 }
--- composeApp/src/commonMain/kotlin/kz/dehaliboch/decomposesample/App.kt
+++ composeApp/src/commonMain/kotlin/kz/dehaliboch/decomposesample/App.kt
@@ -2,12 +2,30 @@ package kz.dehaliboch.decomposesample
 
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import com.arkivanov.decompose.extensions.compose.stack.Children
+import com.arkivanov.decompose.extensions.compose.stack.animation.slide
+import com.arkivanov.decompose.extensions.compose.stack.animation.stackAnimation
+import com.arkivanov.decompose.extensions.compose.subscribeAsState
+import kz.dehaliboch.decomposesample.navigation.RootComponent
+import kz.dehaliboch.decomposesample.screens.ScreenA
+import kz.dehaliboch.decomposesample.screens.ScreenB
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
 @Preview
-fun App() {
+fun App(root: RootComponent) {
     MaterialTheme {
+        val childStack by root.childStack.subscribeAsState()
 
+        Children(
+            stack = childStack,
+            animation = stackAnimation(slide()),
+        ) { child ->
+            when (val instance = child.instance) {
+                is RootComponent.Child.ScreenA -> ScreenA(instance.component)
+                is RootComponent.Child.ScreenB -> ScreenB(instance.component)
+            }
+        }
     }
 }
